8ca261fb6d9657fe099d2523cd42dc9b5fc1ce8b
Nirjana Barua <nirjanamohona@gmail.com>
2024-11-03 21:22:42 +0600

    nothing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwppk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cwppk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +27,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(uri);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
